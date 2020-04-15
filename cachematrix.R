4313578d19b5cccd199fd06f1933b149500f97cd
anne-qwas <63745096+anne-qwas@users.noreply.github.com>
2020-04-15 22:09:38 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions creates a temporary memory position for the calculated
+## inverse of a matrix, such that this will not have to be calculated again
+## during subsquent actions.
 
-## Write a short comment describing this function
+## This function translates your square matrix into a special 'list'
+## matrix with the environment needed to enable the temporary memory position.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function either returns the already calculated inverse matrix, or
+## calculates the inverse of the given square matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
